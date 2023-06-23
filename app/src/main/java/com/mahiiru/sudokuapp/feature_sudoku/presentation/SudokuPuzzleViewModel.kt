b3f443548e44cdd8df573fbdc8b22b392c409e7f
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-06-23 19:56:55 -0400

    Dagger Hilt implemented. ViewModel SetUp

--- app/src/main/java/com/mahiiru/sudokuapp/feature_sudoku/presentation/SudokuPuzzleViewModel.kt
+++ app/src/main/java/com/mahiiru/sudokuapp/feature_sudoku/presentation/SudokuPuzzleViewModel.kt
@@ -4,8 +4,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mahiiru.sudokuapp.core.util.Resource
+import com.mahiiru.sudokuapp.feature_sudoku.domain.model.SudokuPuzzle
 import com.mahiiru.sudokuapp.feature_sudoku.domain.repository.ISudokuRepository
+import com.mahiiru.sudokuapp.feature_sudoku.domain.use_case.BuildPuzzleUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -20,7 +25,60 @@ class SudokuPuzzleViewModel @Inject constructor(
         getSudokuPuzzle()
     }
 
-    private fun getSudokuPuzzle() {
-        TODO("Not yet implemented")
+    fun onEvent(event: SudokuPuzzleEvent){
+        when (event){
+            is SudokuPuzzleEvent.OnInput -> TODO()
+            is SudokuPuzzleEvent.OnNewSudokuPuzzle -> TODO()
+            is SudokuPuzzleEvent.OnSolveSudoku -> TODO()
+            is SudokuPuzzleEvent.OnSudokuCompleted -> TODO()
+            is SudokuPuzzleEvent.OnTileFocused -> TODO()
+        }
     }
+
+    private fun getSudokuPuzzle() {
+        viewModelScope.launch {
+            repository.getCurrentGame().collect { result ->
+                when (result) {
+                    is Resource.Error -> {
+                        buildNewPuzzle()
+                    }
+                    is Resource.Loading -> {
+                        state = state.copy(isLoading = result.isLoading)
+                    }
+                    is Resource.Success -> {
+                        state = state.copy(puzzle = getPuzzleFromData(result.data!!))
+                    }
+                }
+            }
+        }
+    }
+
+    private suspend fun buildNewPuzzle() {
+        val newPuzzle = SudokuPuzzle(state.boundary)
+        BuildPuzzleUseCase().invoke(newPuzzle)
+        repository.updateGame(newPuzzle).collect{ result ->
+            when (result) {
+                is Resource.Error -> Unit
+                is Resource.Loading -> state.copy(isLoading = result.isLoading)
+                is Resource.Success -> state.copy(puzzle = getPuzzleFromData(result.data!!))
+            }
+        }
+    }
+
+    private fun getPuzzleFromData(data: SudokuPuzzle): HashMap<Int, SudokuTile> {
+        val newHash: HashMap<Int, SudokuTile> = HashMap()
+        data.graph.forEach {
+            val node = it.value[0]
+            newHash[it.key] = SudokuTile(
+                node.x,
+                node.y,
+                node.color,
+                false,
+                node.readOnly
+            )
+        }
+        return newHash
+    }
+
+
 }
