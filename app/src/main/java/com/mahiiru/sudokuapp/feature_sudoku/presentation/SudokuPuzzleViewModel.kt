e1e745bca2bb831c93856ddb65325c6bd0eedd41
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-06-25 12:45:50 -0400

    BuildPuzzle and SolvePuzzle implemented. ViewModel implemented. Compose UI implemented.

--- app/src/main/java/com/mahiiru/sudokuapp/feature_sudoku/presentation/SudokuPuzzleViewModel.kt
+++ app/src/main/java/com/mahiiru/sudokuapp/feature_sudoku/presentation/SudokuPuzzleViewModel.kt
@@ -10,6 +10,8 @@ import com.mahiiru.sudokuapp.core.util.Resource
 import com.mahiiru.sudokuapp.feature_sudoku.domain.model.SudokuPuzzle
 import com.mahiiru.sudokuapp.feature_sudoku.domain.repository.ISudokuRepository
 import com.mahiiru.sudokuapp.feature_sudoku.domain.use_case.BuildPuzzleUseCase
+import com.mahiiru.sudokuapp.feature_sudoku.domain.use_case.IsPuzzleCompletedUseCase
+import com.mahiiru.sudokuapp.feature_sudoku.domain.use_case.SolveSudokuUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -29,10 +31,59 @@ class SudokuPuzzleViewModel @Inject constructor(
     fun onEvent(event: SudokuPuzzleEvent){
         when (event){
             is SudokuPuzzleEvent.OnInput -> TODO()
-            is SudokuPuzzleEvent.OnNewSudokuPuzzle -> TODO()
-            is SudokuPuzzleEvent.OnSolveSudoku -> TODO()
-            is SudokuPuzzleEvent.OnSudokuCompleted -> TODO()
+            is SudokuPuzzleEvent.OnNewSudokuPuzzle -> rebuildNewPuzzle()
+            is SudokuPuzzleEvent.OnSolveSudoku -> solvePuzzle()
+            is SudokuPuzzleEvent.OnSudokuCompleted -> validatePuzzle()
             is SudokuPuzzleEvent.OnTileFocused -> TODO()
+        }
+    }
+
+    private fun validatePuzzle() {
+        viewModelScope.launch {
+            repository.getCurrentGame().collect{ result ->
+                when (result) {
+                    is Resource.Error -> buildNewPuzzle()
+                    is Resource.Loading -> state = state.copy(isLoading = result.isLoading)
+                    is Resource.Success -> {
+                        state = state
+                            .copy(isPuzzleCompleted = IsPuzzleCompletedUseCase().invoke(result.data!!))
+                    }
+                }
+            }
+        }
+    }
+    private fun solvePuzzle() {
+        viewModelScope.launch {
+            repository.getCurrentGame().collect{ result ->
+                when (result) {
+                    is Resource.Error -> buildNewPuzzle()
+                    is Resource.Loading -> state = state.copy(isLoading = result.isLoading)
+                    is Resource.Success -> solveSudokuPuzzle(result.data!!)
+                }
+            }
+        }
+    }
+
+    private suspend fun solveSudokuPuzzle(data: SudokuPuzzle) {
+        SolveSudokuUseCase().invoke(data)
+        repository.updateGame(data).collect{ result ->
+            when (result) {
+                is Resource.Error -> buildNewPuzzle()
+                is Resource.Loading -> state = state.copy(isLoading = result.isLoading)
+                is Resource.Success -> state = state.copy(puzzle = getPuzzleFromData(result.data!!))
+            }
+        }
+    }
+
+    private fun rebuildNewPuzzle() {
+        viewModelScope.launch {
+            repository.getCurrentGame().collect{ result ->
+                when (result) {
+                    is Resource.Error -> buildNewPuzzle()
+                    is Resource.Loading -> state = state.copy(isLoading = result.isLoading)
+                    is Resource.Success -> buildNewPuzzle()
+                }
+            }
         }
     }
 
@@ -40,15 +91,11 @@ class SudokuPuzzleViewModel @Inject constructor(
         viewModelScope.launch {
             repository.getCurrentGame().collect { result ->
                 when (result) {
-                    is Resource.Error -> {
-                        buildNewPuzzle()
-                    }
-                    is Resource.Loading -> {
-                        state = state.copy(isLoading = result.isLoading)
-                    }
-                    is Resource.Success -> {
-                        state = state.copy(puzzle = getPuzzleFromData(result.data!!))
-                    }
+                    is Resource.Error -> buildNewPuzzle()
+                    is Resource.Loading -> state = state.copy(isLoading = result.isLoading)
+
+                    is Resource.Success -> state = state.copy(puzzle = getPuzzleFromData(result.data!!))
+
                 }
             }
         }
@@ -60,8 +107,9 @@ class SudokuPuzzleViewModel @Inject constructor(
         repository.updateGame(newPuzzle).collect{ result ->
             when (result) {
                 is Resource.Error -> Unit
-                is Resource.Loading -> state.copy(isLoading = result.isLoading)
-                is Resource.Success -> state.copy(puzzle = getPuzzleFromData(result.data!!))
+                is Resource.Loading -> state = state.copy(isLoading = result.isLoading)
+                is Resource.Success -> state = state.copy(puzzle = getPuzzleFromData(result.data!!))
+
             }
         }
     }
